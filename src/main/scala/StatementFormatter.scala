42d06b678e43b8279ef0395fc9bf8f306cd489c8
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-08-01 15:33:10 +0100

    refactor: tail recursive formatTransactions

--- src/main/scala/StatementFormatter.scala
+++ src/main/scala/StatementFormatter.scala
@@ -1,20 +1,32 @@
+import scala.annotation.tailrec
+import scala.collection.mutable
+
 object StatementFormatter {
   val StatementHeader: String = formatLine("Amount", "Date", "Balance")
 
   def format: Seq[Transaction] => String =
     sortTransactions _ andThen(
-      formatTransactions(0) andThen
+      formatTransactions _ andThen
         addHeader
     )
 
   private def sortTransactions(txns: Seq[Transaction]) =
     txns.sortBy(tx => tx.date)
 
-  private def formatTransactions: Int => Seq[Transaction] => String = (startingBalance: Int) => {
-    case Nil => ""
-    case tx :: txs =>
-      formatTransactions(startingBalance + tx.amount)(txs)
-        .concat(formatLine(tx.amount, tx.date, startingBalance + tx.amount))
+
+  private def formatTransactions(transactions: Seq[Transaction]): String = {
+    val transactionsReversed = transactions.reverse
+    @tailrec def recursiveFormatter(statementBuilder: mutable.StringBuilder, startingBalance: Int, transactionsCountDown: Int): String = {
+      if (transactionsCountDown == 0) statementBuilder.toString
+      else {
+        val currentTransaction = transactionsReversed(transactionsCountDown - 1)
+        recursiveFormatter(
+          statementBuilder.addString(new mutable.StringBuilder(formatLine(currentTransaction.amount, currentTransaction.date, startingBalance + currentTransaction.amount))),
+          startingBalance + currentTransaction.amount,
+          transactionsCountDown - 1)
+      }
+    }
+    recursiveFormatter(new mutable.StringBuilder(), 0, transactions.length)
   }
 
   private def addHeader(formattedTxns: String): String = StatementHeader.concat(formattedTxns)

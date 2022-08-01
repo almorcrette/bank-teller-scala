72ec348c52dbf1e1564781731cd805e09268e958
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-08-01 17:40:54 +0100

    test & implement: Ledger functional only

--- src/test/scala/LedgerSpec.scala
+++ src/test/scala/LedgerSpec.scala
@@ -9,34 +9,37 @@ class LedgerSpec extends AnyWordSpec with Matchers {
 
   "A Bank Ledger" should {
     "deposit once and retrieve a balance" in {
-      val ledger = new Ledger()
-        .deposit(1000, Day1)
-      ledger.balance shouldEqual 1000
+      LedgerTools.balance(
+        LedgerTools.deposit(1000, Day1)()
+      ) shouldEqual 1000
     }
     "deposit multiple times and retrieve a balance" in {
-      val ledger = new Ledger()
-        .deposit(1000, Day1)
-        .deposit(2000, Day2)
-      ledger.balance shouldEqual 3000
+      LedgerTools.balance(
+        LedgerTools.deposit(2000, Day2)(
+          LedgerTools.deposit(1000, Day1)()
+        )
+      ) shouldEqual 3000
+
     }
+
     "deposit and withdraw and maintain correct balance" in {
-      val ledger = new Ledger()
-        .deposit(1000, Day1)
-        .withdraw(499, Day2)
-      ledger.balance shouldEqual 501
+      LedgerTools.balance(
+        LedgerTools.withdraw(499, Day2)(
+          LedgerTools.deposit(1000, Day1)()
+        )
+      ) shouldEqual 501
     }
+
     "deposit and withdraw and maintain correct balance at each transaction" in {
-      val ledgerToday = new Ledger()
-        .deposit(1000, Day1)
-      val ledgerTomorrow = ledgerToday
-        .withdraw(499, Day2)
-      ledgerToday.balance shouldEqual 1000
-      ledgerTomorrow.balance shouldEqual 501
+      val ledgerToday = LedgerTools.deposit(1000, Day1)()
+      val ledgerTomorrow = LedgerTools.withdraw(499, Day2)(ledgerToday)
+      LedgerTools.balance(ledgerToday) shouldEqual 1000
+      LedgerTools.balance(ledgerTomorrow) shouldEqual 501
     }
     "deposit and withdraw and log transactions" in {
-      val ledger = new Ledger()
-        .deposit(1000, Day1)
-        .withdraw(499, Day2)
+      val ledger = LedgerTools.withdraw(499, Day2)(
+        LedgerTools.deposit(1000, Day1)()
+      )
       ledger.transactions(0).amount shouldEqual 1000
       ledger.transactions(0).date shouldEqual Day1
       ledger.transactions(1).amount shouldEqual -499
